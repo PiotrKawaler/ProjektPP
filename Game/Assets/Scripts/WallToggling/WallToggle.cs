e4d5322566b2bff85fbc2f39cd25c7468b3aa886
Mmmusial <43841310+Mmmusial@users.noreply.github.com>
2021-09-06 20:08:16 +0200

    WallTrap
    Added example wall on TurtleScene

---
+++ Game/Assets/Scripts/WallToggling/WallToggle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+namespace WallToggling
+{
+    public class WallToggle : MonoBehaviour
+    {
+        [SerializeField] private float reenableDelay;
+        [SerializeField] private Wall[] walls;
+        
+        private const string PlayerTag = "Player";
+        private const string EnemyTag = "Enemy";
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (ShouldInteractWith(other))
+            {
+                DisableWalls();
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (ShouldInteractWith(other))
+            {
+                Invoke(nameof(EnableWalls), reenableDelay);
+            }
+        }
+
+        private void DisableWalls()
+        {
+            foreach (Wall wall in walls)
+            {
+                wall.Disable();
+            }
+        }
+
+        private void EnableWalls()
+        {
+            foreach (Wall wall in walls)
+            {
+                wall.Enable();
+            }
+        }
+
+        private bool ShouldInteractWith(Component other)
+        {
+            return other.CompareTag(PlayerTag) || other.CompareTag(EnemyTag);
+        }
+    }
+}
