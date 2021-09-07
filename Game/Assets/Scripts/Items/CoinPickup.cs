a88ae358e9177c420594446c13b921f0da37a8b2
JakubSob1997 <jakubsob97@gmail.com>
2021-09-07 15:51:52 +0200

    Level skończony

--- Game/Assets/Scripts/Items/CoinPickup.cs
+++ Game/Assets/Scripts/Items/CoinPickup.cs
@@ -7,12 +7,21 @@ public class CoinPickup : MonoBehaviour
     
     void OnTriggerEnter2D(Collider2D collider)
     {
-        ScoreTextScript.coinCount++;
-        var colider = GetComponent<Collider2D>();
-        if (colider != null)
+
+        var id = collider?.attachedRigidbody.GetComponent<Identifier>();
+        if (id == null) return;
+
+        if(id.Identity == Identity.Player)
         {
-            colider.enabled = false;
+            ScoreTextScript.coinCount++;
+            var colider = GetComponent<Collider2D>();
+            if (colider != null)
+            {
+                colider.enabled = false;
+            }
+            Destroy(gameObject);
         }
-        Destroy(gameObject);
+
+        
     }
 }
